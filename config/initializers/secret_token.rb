4f58846fee179c37f421926be6d114e878c5bca9
Rich Niles <richkniles@gmail.com>
2012-07-04 21:22:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreedApp::Application.config.secret_token = '4ef8ef9e622aba9d4e4221ec4f029325420b65d43deb244d19ec4a1d2b5b46ccaaa43d04218bc4e986d02db3159bb87f49fa8d50172e0b27554c398da759f8b1'
